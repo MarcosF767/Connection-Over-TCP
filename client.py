d8ea7877f5a5d594feb88e0f27e2d20da8ac89e2
MarcosF767 <marfer737@gmail.com.com>
2021-09-10 23:41:00 -0400

    Added headers

--- client.py
+++ client.py
@@ -29,15 +29,29 @@ f.seek(0, os.SEEK_END)
 size_of_file = f.tell()
 print(size_of_file)
 
+header = "Content-Disposition: attachment; filename=\"%s\"\r\nContent-Type: " % FILE + \
+    "application/octet-stream\r\nContent-Length: %s\r\n\r\n" % str(size_of_file)
+#print(header)
+
+try:
+    soc.send(bytes(header, 'UTF-8'))
+except:
+    sys.stderr.write("ERROR: Connection disconnected while sending the headers.\n")
+    f.close()
+    soc.close()
+    exit(1)
+
 while(read_pointer < size_of_file):
     f.seek(read_pointer)
     try:
         sent = soc.send(f.read(10000))
-        print("sent " + str(sent) + " bytes")
+        #print("sent " + str(sent) + " bytes")
     except:
         sys.stderr.write("ERROR: Connection disconnected while sending the file.\n")
+        f.close()
         soc.close()
         exit(1) #sys.exit(1)
     read_pointer += 10000
-    
+
+f.close()
 soc.close()
