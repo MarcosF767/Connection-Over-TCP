78eba75d6399903273b19ad4d1bf8051122c7295
MarcosF767 <marfer737@gmail.com.com>
2021-09-10 21:38:37 -0400

    Accepting accio

--- client.py
+++ client.py
@@ -9,12 +9,17 @@ FILE = sys.argv[3]
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+in_message = ""
+
 try:
     soc.connect((HOST, int(PORT)))
 except:
     sys.stderr.write("ERROR: Not able to connect to the provided host and port.\n")
-    sys.exit(1)
+    exit(1)
 
-sys.stderr.write("success")
+while(not("accio\r\n" in in_message)):
+    recieved = soc.recv(1024)
+    in_message = in_message + recieved.decode('utf-8')
 
+print(f"recieved {in_message}")
 soc.close()
