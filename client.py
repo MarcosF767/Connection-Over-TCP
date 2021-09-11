8d9705efa8fea396448b4d507c382e8724a23c77
MarcosF767 <marfer737@gmail.com.com>
2021-09-10 21:06:48 -0400

    Error handling

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import socket
 
 HOST = sys.argv[1]
 PORT = sys.argv[2]
@@ -8,6 +9,12 @@ FILE = sys.argv[3]
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-soc.connect((HOST, PORT))
+try:
+    soc.connect((HOST, int(PORT)))
+except:
+    sys.stderr.write("ERROR: Not able to connect to the provided host and port.\n")
+    sys.exit(1)
+
+sys.stderr.write("success")
 
 soc.close()
